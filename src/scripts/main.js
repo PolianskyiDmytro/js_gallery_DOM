e6ef5caf32ff25d10ff5aa65b1c8db104b48390c
Polianskyi Dmytro <dimdemon1@gmail.com>
2025-04-19 19:43:02 +0300

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,17 @@
 'use strict';
+
+const thumbs = document.querySelector('#thumbs');
+const largeImg = document.querySelector('#largeImg');
+
+thumbs.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  const a = e.target.closest('a');
+
+  if (!a) {
+    return;
+  }
+
+  largeImg.src = a.href;
+  largeImg.alt = a.title;
+});
