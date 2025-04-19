22991d1cabd0183288caaac170c2118d6df90b0b
Polianskyi Dmytro <dimdemon1@gmail.com>
2025-04-19 19:51:50 +0300

    make build

---
+++ index.29b4d981.js
@@ -0,0 +1,2 @@
+var e=document.querySelector("#thumbs"),t=document.querySelector("#largeImg");e.addEventListener("click",function(e){var r=e.target.closest("a");r&&(e.preventDefault(),t.src=r.href,t.alt=r.title)});
+//# sourceMappingURL=index.29b4d981.js.map
